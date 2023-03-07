f961de23c66a116f7badfefd8024e6d6fbf3cd3b
Mustafa Atik <mustafaatik76@gmail.com>
2023-03-07 23:28:43 +0300

    regression update

--- src/test/java/cydeo/utilities/Driver.java
+++ src/test/java/cydeo/utilities/Driver.java
@@ -1,15 +1,20 @@
 package cydeo.utilities;
 
-import io.github.bonigarcia.wdm.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.edge.*;
-import org.openqa.selenium.firefox.*;
-import org.openqa.selenium.remote.*;
+import cydeo.utilities.ConfigurationReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.*;
-import java.util.*;
-import java.util.concurrent.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -46,16 +51,10 @@ public class Driver {
                 switch statement will determine the case, and open the matching browser
             */
             switch (browserType){
-
                 case "chrome":
 
-                    ChromeOptions options = new ChromeOptions();
-                    Map<String, Object> prefs = new HashMap<String, Object>();
-                    prefs.put("intl.accept_languages", "en-GB");
-                    options.setExperimentalOption("prefs", prefs);
                     WebDriverManager.chromedriver().setup();
-                    //WebDriver driver = new ChromeDriver(options);
-                    driverPool.set(new ChromeDriver(options));
+                    driverPool.set(new ChromeDriver());
                     driverPool.get().manage().window().maximize();
                     driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
@@ -67,7 +66,7 @@ public class Driver {
                     break;
                 case "remote-chrome":
                     // assign your grid server address
-                    String gridAdress = "54.89.242.106"; // put your own Linux grid IP here
+                    String gridAdress = "54.160.69.233"; // put your own Linux grid IP here
                     try {
                         URL url = new URL("http://"+gridAdress+":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
@@ -78,7 +77,7 @@ public class Driver {
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
                     }
-                 break;
+                    break;
                 case "saucelab-chrome":
                     try{
                         URL url = new URL("https://oauth-sdetoscar-844c8:66e7117f-390e-4556-8105-07af96a01f7a@ondemand.eu-central-1.saucelabs.com:443/wd/hub");
@@ -91,7 +90,7 @@ public class Driver {
                         e.printStackTrace();
                     }
 
-            break;
+                    break;
                 case "saucelab-edge":
                     EdgeOptions browserOptions = new EdgeOptions();
                     browserOptions.setCapability("platformName", "Windows 11");
